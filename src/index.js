aa16d9a2fa06b95b4faf52c16e3d0bbb3e660a10
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2021-02-07 15:30:07 +0100

    interactive messages

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createEventAdapter } = require('@slack/events-api');
+const { createMessageAdapter } = require("@slack/interactive-messages")
 const { WebClient } = require('@slack/web-api');
 const { DB_URL, PORT, SIGNING_SECRET, OAUTH_TOKEN, BOT_USER_TOKEN } = require('./config/config');
 const port = process.env.PORT || PORT || 3000;
 
 const web = new WebClient(BOT_USER_TOKEN);
 const slackEvents = createEventAdapter(SIGNING_SECRET);
-
-// Create an express application
+const slackInteractions = createMessageAdapter(SIGNING_SECRET);
 const app = express();
 
-// Plug the adapter in as a middleware
 app.use('/slack/events', slackEvents.requestListener());
 app.use(
   bodyParser.json({
@@ -38,10 +37,22 @@ slackEvents.on('app_mention', async (event) => {
   console.log(`Received a mention event`);
   try {
     // Use the `chat.postMessage` method to send a message from this app
+
     await web.chat.postMessage({
       channel: event.channel,
       text: `Hey <@${event.user}> you buzzed me`,
     });
+    slackInteractions.action('welcome_button', (payload, respond) => {
+      console.log({ payload, responsd });
+      respond({
+        text: 'Success!',
+      });
+
+      return {
+        text: 'Processing...',
+      }
+    });
+
   } catch (error) {
     console.log(error);
   }
