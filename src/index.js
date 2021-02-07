7a07a43026b9ad69ae311d52e3054c12382a3f83
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2021-02-07 14:27:39 +0100

    event listener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 
-const { DB_URL, PORT, SIGNING_SECRET, OAUTH_TOKEN, BOT_USER_TOKEN } = require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
+const { DB_URL, PORT, SIGNING_SECRET, OAUTH_TOKEN, BOT_USER_TOKEN } = require('./config/config');
 const port = process.env.PORT || PORT || 3000;
+
+const web = new WebClient(BOT_USER_TOKEN);
 const slackEvents = createEventAdapter(SIGNING_SECRET);
 
 // Create an express application
@@ -27,40 +30,24 @@ app.post("/slack/events", (req, res) => {
   console.log(req.body);
 
 })
-slackEvents.on('message', (event) => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-  console.log({ event })
-});
-slackEvents.on('app_mention', (event) => {
+// slackEvents.on('message', (event) => {
+//   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+//   console.log({ event })
+// });
+slackEvents.on('app_mention', async (event) => {
   console.log(`Received a mention event`);
-
+  try {
+    // Use the `chat.postMessage` method to send a message from this app
+    await web.chat.postMessage({
+      channel: event.channel,
+      text: `Hey ${event.user} you buzzed me`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
   console.log({ event })
 });
 (async () => {
   const server = await slackEvents.start(app.listen(port));
   console.log(`Listening for events on ${server.address().port}`);
-})();
-
-
-
-
-const { WebClient } = require('@slack/web-api');
-// Create a new instance of the WebClient class with the token read from your environment variable
-const web = new WebClient(BOT_USER_TOKEN);
-// The current date
-const currentTime = new Date().toTimeString();
-
-(async () => {
-
-  try {
-    // Use the `chat.postMessage` method to send a message from this app
-    await web.chat.postMessage({
-      channel: '#general',
-      text: `The current time is ${currentTime}`,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-
-  console.log('Message posted!');
 })();
